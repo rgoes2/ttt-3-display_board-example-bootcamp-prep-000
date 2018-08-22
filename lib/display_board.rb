857b02eec5fd1fff6731f0c55baea4835c7ea736
Renan Góes <renangoes12@gmail.com>
2018-08-22 00:32:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ def display_board
   puts "#{separation}"
   puts "#{cell}#{row}#{cell}#{row}#{cell}"
   puts "#{separation}"
-  puts "#{cell}#{row}#{cell}#{row}#{cell}"
+  return "#{cell}#{row}#{cell}#{row}#{cell}"
 end
